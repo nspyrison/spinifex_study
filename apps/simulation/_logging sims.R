09dd54da7a24e4050015757e582bbf8922391e59
Nicholas Spyrison <spyrison@gmail.com>
2019-10-04 17:36:35 +1000

    parameter start, lda, mahalonobis dist.

--- apps/simulation/_logging sims.R
+++ apps/simulation/_logging sims.R
@@ -1,6 +1,6 @@
 ## scratchpad for making simulation csv files.
 
-### SCRATCH
+### SCRATCH -----
 #write.csv(mtcars, "mtcars.csv")
 sim1 <- readRDS("./apps/simulation/simulation_data001.rds") 
 str(sim1)
@@ -8,7 +8,8 @@ mncl<-attr(sim1, "mncl")
 colnames(mncl) <- paste0("V", 1:10)
 mncl
 
-### LOAD ALL SIM
+
+### LOAD ALL SIM -----
 load_num <- 1
 load_name <- sprintf("simulation_data%03d", load_num)
 load_file <- paste0("./apps/simulation/", load_name, ".rds") # "./apps/simulation/"
@@ -21,7 +22,8 @@ while (file.exists(load_file)){
 }
 loaded_sim_names <- ls()[grepl("simulation_data", ls())]
 
-### CREATE SIM_CL_MEANS.CSV
+
+### CREATE SIM_CL_MEANS.CSV -----
 df_mncl <- NULL
 for (i in 1:length(loaded_sim_names)) {
   this_sim <- get(loaded_sim_names[i])
@@ -38,6 +40,7 @@ df_mncl
 rownames(df_mncl) <- NULL
 sim_cl_means <- df_mncl
 write.csv2(sim_cl_means, "sim_cl_means.csv")
+
 
 ### CREATE SIM_COVAR.CSV
 df_covar <- NULL
@@ -56,4 +59,45 @@ df_covar
 rownames(df_covar) <- NULL
 sim_covar <- df_covar
 write.csv2(sim_covar, "sim_covar.csv")
+
+
+### CREATE SIM_PARAMETERS.CSV -----
+df_parameters <- NULL
+for (i in 1:length(loaded_sim_names)) {
+  this_sim <- get(loaded_sim_names[i])
+  this_reorder <- attr(this_sim, "col_reorder")
+  this_covar <- attr(this_sim, "vc")[this_reorder, this_reorder]
+  
+  p <- ncol(this_sim)
+  n_cl <- length(attr(this_sim, "ncl"))
+  pnoise <- sum(colSums(attr(this_sim, "mncl") == 0) == n_cl)
+  
+  this_parameters <- data.frame(id = i, p, n_cl, pnoise)
+  df_parameters <- rbind(df_parameters, this_parameters)
+}
+df_parameters
+rownames(df_parameters) <- NULL
+sim_parameters <- df_parameters
+write.csv2(sim_parameters, "sim_parameters.csv")
+
+
+### LDA scratchpad -----
+# https://towardsdatascience.com/linear-discriminant-analysis-lda-101-using-r-6a97217a55a6
+dat <- tourr::flea
+# f <- paste(names(dat)[7], "~", paste(names(dat)[-7], collapse=" + "))
+# MASS::lda(as.formula(f), data = dat)
+lda <- MASS::lda(species~., data = dat)
+# return what? the coeffiecients of the var that make LD1, LD2?
+str(lda)
+lda$scaling
+
+
+###  MAHALONOBIS DIST -----
+# https://stat.ethz.ch/R-manual/R-devel/library/stats/html/mahalanobis.html
+?mahalanobis
+colnames(sim1) <- paste0("V", 1:ncol(sim1))
+rownames(sim1) <- NULL
+mn <- apply(sim1, 2, mean)
+cv <- var(sim1)
+mahalanobis(sim1, mn, cv)
 
