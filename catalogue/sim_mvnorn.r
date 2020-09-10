1a9e8ba2d37542ca2e071538959e56f8149abfbd
Nicholas Spyrison <spyrison@gmail.com>
2020-09-10 14:33:41 +1000

    generalizing simulating; 3310 thru 31010

--- catalogue/sim_mvnorn.r
+++ catalogue/sim_mvnorn.r
@@ -11,58 +11,140 @@
 
 ## Setup
 source("./R/sim_pDim_kCl.r")
-args(sim_pDim_kCl)
-DO_SAVE <- FALSE
+DO_SAVE = FALSE 
 
-## Initializae
-p <- 10    ## Number of dimensions
-k_cl <- 3  ## Number of clusters
-z <- rep(0, p)
+sims_kpsigp <- function(k_cl = 3, ## Number of clusters
+                        p_sig = 3, ## Intrisic data dimensionality, previously p_sig (nal)
+                        p = 10, ## Number of dimensions
+                        n_obs_per_cl = rep(list(200), k_cl)
+                        , DO_SAVE = FALSE 
+){
+  ## Initializae
+  idd <- p_sig - 1
+  models <- c("VII", "EEE", "EVV", "VVV")
+  suffix <- paste0(as.character(c(k_cl, p_sig, p)), collapse = "")
+  cl_lvls <- paste0("cl ", letters[1:k_cl])
+  names(n_obs_per_cl) <- cl_lvls
+  ## Hard-coded parameters
+  signal <- 10
+  var_sz <- 1
+  cor_sz <- .5
+  
+  ## Variable means by cluster
+  diag_p <- mn_mat <- diag(p)
+  diag(mn_mat) <- signal
+  mns <- list()
+  for(i in 1:k_cl) mns[[i]] <- mn_mat[i,]
+  names(mns) <- cl_lvls
+  
+  ## Covariances by cluster
+  cov1 <- cov2 <- cov3 <<- var_sz * diag_p ## cov1 set, init cov2, cov3
+  cov_psig <- cov2[1:p_sig, 1:p_sig]
+  ind <- upper.tri(cov_psig) | lower.tri(cov_psig)
+  cov_psig[ind] <- cor_sz
+  cov2[1:p_sig, 1:p_sig] <- cov_psig ## cov2 set
+  cov_psig[ind] <- -cor_sz
+  cov3[1:p_sig, 1:p_sig] <- cov_psig ## cov3 set
+  ## Ensure cov3 is positive definite 
+  if(lqmm::is.positive.definite(cov3) == FALSE){
+    cov3_pd <- lqmm::make.positive.definite(cov3)
+    if(all.equal(cov3, cov3_pd) != TRUE){
+      warning("Had to make cov3 positive definite. From, to, respectively")
+      print(cov3)
+      print(cov3_pd)
+    }
+    cov3 <- cov3_pd
+  }
+  # ## EVALUATING POSITIVE DEFINATE.
+  # browser()
+  # lqmm::is.positive.definite(cov3)
+  # z <- lqmm::make.positive.definite(cov3)
+  # lqmm::is.positive.definite(z)
+  # all.equal(z, cov3)
+  # i <- 0
+  # val <- -999
+  # while(lqmm::is.positive.definite(cov3) == F){
+  #   val <- -(cor_sz - (.1 * i))
+  #   cov_psig[ind] <- val
+  #   cov3[1:p_sig, 1:p_sig] <- cov_psig ## cov3 set
+  #   print(paste0(val, " doesn't lead to cov3 being positive definate."))
+  #   cov3
+  #   i <- i + 1
+  # }
+  # print(paste0(val, " DOES lead to cov3 being positive definate."))
+  
+  covs_VII <- list(cov1 / 4, cov1, cov1 * 4)
+  covs_EEE <- list(cov2, cov2, cov2)
+  covs_EVV <- list(cov1, cov2, cov3)
+  ##
+  diag(cov1) <- diag(cov1) / 4
+  diag(cov2) <- diag(cov3) * 4
+  covs_VVV <- list(cov1, cov2, cov3)
+  names(covs_VII) <- cl_lvls
+  names(covs_EEE) <- cl_lvls
+  names(covs_EVV) <- cl_lvls
+  names(covs_VVV) <- cl_lvls
+  
+  ## Create simulations
+  obj_nms <- paste0(models, "_", suffix)
+  cov_nms <- paste0("covs_", models)
+  
+  for(i in 1:length(obj_nms)){
+    this_sim <- sim_pDim_kCl(means = mns, sigmas = get(cov_nms[i]),
+                             cl_points = n_obs_per_cl, do_shuffle = F)
+    assign(x = obj_nms[i], value = this_sim, envir = globalenv())
+    print(paste0("Assigned ", obj_nms[i], " as a global variable."))
+  }
+  
+  # ## Save
+  # if(DO_SAVE == TRUE){
+  #   root <- "./catalogue/simulations/"
+  #   save(VII_3310, file = paste0(root, quote(VII_3310), ".rda"))
+  #   save(EEE_3310, file = paste0(root, quote(EEE_3310), ".rda"))
+  #   save(EVV_3310, file = paste0(root, quote(EVV_3310), ".rda"))
+  #   save(VVV_3310, file = paste0(root, quote(VVV_3310), ".rda"))
+  # }
+}
+sims_kpsigp(k_cl = 3, p_sig =  3,  p =  10)
+sims_kpsigp(k_cl = 3, p_sig =  5,  p =  10)
+sims_kpsigp(k_cl = 3, p_sig =  7,  p =  10)
+sims_kpsigp(k_cl = 3, p_sig =  10, p =  10)
 
-## Obs, and means 
-n_obs_per_cl <- rep(list(200), k_cl)
-mns <- list(c(5, rep(0, p - 1)), 
-            c(0, 5, rep(0, p - 2)),
-            c(0, 0, 5, rep(0, p - 3))
-)
-
-## Covariances
-diag_p <- diag(p)
-ind <- upper.tri(diag_p)
-cov1 <- cov2 <- cov3 <- .6 * diag_p
-cov2[2, 1] <- cov2[3, 1] <- cov2[3, 2] <- .5 
-cov2[ind] <- t(cov2)[ind]
-cov3[2, 1] <- cov3[3, 1] <- cov3[3, 2] <- -.5 
-cov3[ind] <- t(cov3)[ind]
-
-covs_VII <- list(cov1, 2 * cov1, .5 * cov1)
-covs_EEE <- list(cov2, cov2, cov2)
-covs_VVV <- list(cov1, cov2, cov3)
-
-## Create simulations
-sim_VII <- sim_pDim_kCl(means = mns, sigmas = covs_VII, 
-                        cl_points = n_obs_per_cl, do_shuffle = F)
-sim_EEE <- sim_pDim_kCl(means = mns, sigmas = covs_EEE, 
-                        cl_points = n_obs_per_cl, do_shuffle = F)
-sim_VVV <- sim_pDim_kCl(means = mns, sigmas = covs_VVV, 
-                        cl_points = n_obs_per_cl, do_shuffle = F)
-
-## Save
 if(DO_SAVE == TRUE){
   root <- "./catalogue/simulations/"
-  save(sim_VII, file = paste0(root, quote(sim_VII), ".rda"))
-  save(sim_EEE, file = paste0(root, quote(sim_EEE), ".rda"))
-  save(sim_VVV, file = paste0(root, quote(sim_VVV), ".rda"))
+  ## 3310
+  save(VII_3310, file = paste0(root, quote(VII_3310), ".rda"))
+  save(EEE_3310, file = paste0(root, quote(EEE_3310), ".rda"))
+  save(EVV_3310, file = paste0(root, quote(EVV_3310), ".rda"))
+  save(VVV_3310, file = paste0(root, quote(VVV_3310), ".rda"))
+  ## 3510
+  save(VII_3510, file = paste0(root, quote(VII_3510), ".rda"))
+  save(EEE_3510, file = paste0(root, quote(EEE_3510), ".rda"))
+  save(EVV_3510, file = paste0(root, quote(EVV_3510), ".rda"))
+  save(VVV_3510, file = paste0(root, quote(VVV_3510), ".rda"))
+  ## 3710
+  save(VII_3710, file = paste0(root, quote(VII_3710), ".rda"))
+  save(EEE_3710, file = paste0(root, quote(EEE_3710), ".rda"))
+  save(EVV_3710, file = paste0(root, quote(EVV_3710), ".rda"))
+  save(VVV_3710, file = paste0(root, quote(VVV_3710), ".rda"))
+  ## 31010
+  save(VII_31010, file = paste0(root, quote(VII_31010), ".rda"))
+  save(EEE_31010, file = paste0(root, quote(EEE_31010), ".rda"))
+  save(EVV_31010, file = paste0(root, quote(EVV_31010), ".rda"))
+  save(VVV_31010, file = paste0(root, quote(VVV_31010), ".rda"))
 }
 
-## Inital vis
-clas <- attr(sim_VII, "cl_lvl") ## all the same number and order of obs
 
-GGally::ggpairs(sim_VII[, 1:3], ggplot2::aes(color = as.factor(clas)))
-GGally::ggpairs(sim_EEE[, 1:3], ggplot2::aes(color = as.factor(clas)))
-GGally::ggpairs(sim_VVV[, 1:3], ggplot2::aes(color = as.factor(clas)))
+## Inital vis
+clas <- attr(VII_3310, "cl_lvl") ## all the same number and order of obs
+
+require("GGally"); require("ggplot2");
+GGally::ggpairs(VII_3310[, 1:3], aes(color = as.factor(clas))) + ggtitle("Model VII ggpairs (original variables)")
+GGally::ggpairs(EEE_3310[, 1:3], aes(color = as.factor(clas))) + ggtitle("Model EEE ggpairs (original variables)")
+GGally::ggpairs(EVV_3310[, 1:3], aes(color = as.factor(clas))) + ggtitle("Model EVV ggpairs (original variables)")
+GGally::ggpairs(VVV_3310[, 1:3], aes(color = as.factor(clas))) + ggtitle("Model VVV ggpairs (original variables)")
 
 source("./R/ggpcapairs.r")
-ggpcapairs(sim_VII, class = clas, top_n = 3)
-ggpcapairs(sim_EEE, class = clas, top_n = 3)
-ggpcapairs(sim_VVV, class = clas, top_n = 3)
+ggpcapairs(VII_3310, class = clas, top_n = 3)
+ggpcapairs(EEE_3310, class = clas, top_n = 3)
+ggpcapairs(VVV_3310, class = clas, top_n = 3)
