bf296dc9b738c4b6d6114d1912342e17ff4283f8
Nick Spyrison <spyrison@gmail.com>
2021-02-10 15:50:49 +1100

    Finish resp tbl image, including R/ggproto_pca_biplot.r

--- paper/R/ggproto_ans_plot.r
+++ paper/R/ggproto_ans_plot.r
@@ -1,45 +1,51 @@
 #' @example
 #' str(dat) 
-#' palette(RColorBrewer::brewer.pal(12, "Dark2"))
-#'ggplot(dat) +
-#'    ggproto_ans_plot(dat) +
-#'    facet_wrap(vars(sim_nm)) + theme_minimal()
+#' ggplot(dat) +
+#'   ggproto_ans_plot(dat) +
+#'   facet_wrap(vars(sim_nm)) + theme_minimal()
 
 ## ggproto for the ans_plot
-ggproto_ans_plot <- function(resp_ans_longer){
+ggproto_ans_plot <- function(
+  resp_ans_longer, 
+  brewer_pal = RColorBrewer::brewer.pal(8L, "Dark2")){
   ## List of ggproto objects
   lab_fill <- "Varaiable cluster seperation"
   ret <- list(
     ## Boxplot, signal
-    geom_bar(aes(x = var_num, y = signal, fill = lab_fill),
-             resp_ans_longer, position = "dodge", stat = "identity",
-             width = .5),
+    ggplot2::geom_bar(ggplot2::aes(x = var_num, y = signal, fill = lab_fill),
+                      resp_ans_longer, position = "dodge", stat = "identity",
+                      width = .5),
     
     ## Titles and colors
-    labs(x = "Variable number", y = "Value"),
-    theme(legend.position = "bottom",
-          legend.direction = "vertical"),
-    scale_fill_manual(
-      values = c(palette()[1], "grey80", "lightblue"), name = "",
+    ggplot2::labs(x = "Variable number", y = "Value"),
+    ggplot2::theme(legend.position = "bottom",
+                   legend.direction = "horizontal"),
+    ggplot2::scale_fill_manual(
+      values = c(brewer_pal[1L], "grey80", "lightblue"), name = "",
       labels = c("Varaiable cluster seperation", "selected", "not selected")),
-    scale_colour_manual(values = c("green", "red"),
-                        name = "", labels = c("marks (+)", "marks (-)"))
+    ggplot2::scale_colour_manual(values = c("green", "red"),
+                                 name = "", labels = c("marks (+)", "marks (-)"))
   )
   
   ## Add in the bar and weight
-  p <- 6#<- length(unique(resp_ans_longer$var_num))
-  mark_col <- dplyr::if_else(sign(resp_ans_longer$diff) == 1, "green", "red")
-  ret <- c(ret, 
+  p <- 6L #<- length(unique(resp_ans_longer$var_num))
+  mark_col <- dplyr::if_else(sign(resp_ans_longer$diff) == 1L, "green", "red")
+  ret <- c(ret,
            list(
+             ## 0 line
+             ggplot2::geom_hline(yintercept = 0L, size = 1L, linetype = 2L),
              ## Uniform bar, and text
-             geom_hline(aes(yintercept = bar), resp_ans_longer, size = 1), 
-             geom_text(aes(x = p + 1, y = bar + .1, 
-                           label = paste0("1/p = ", round(bar, 2))),
-                       size = 4, hjust = 1),
+             ggplot2::geom_hline(ggplot2::aes(yintercept = bar), 
+                                 resp_ans_longer, 
+                                 size = 1L, linetype = 3L),
+             ggplot2::geom_text(ggplot2::aes(x = p + 1L, y = bar + .1,
+                                             label = paste0("1/p = ", round(bar, 2))),
+                                resp_ans_longer,
+                                size = 4L, hjust = 1L),
              ## Marks segment
-             geom_segment(aes(x = var_num, xend = var_num,
-                              y = bar, yend = weight + bar),
-                          resp_ans_longer, colour = mark_col, size = 2)
+             ggplot2::geom_segment(ggplot2::aes(x = var_num, xend = var_num,
+                                                y = 0, yend = weight),
+                                   resp_ans_longer, colour = mark_col, size = 2L)
            )
   )
   
--- paper/R/pivot_longer_resp_ans_tbl.r
+++ paper/R/pivot_longer_resp_ans_tbl.r
@@ -1,84 +1,84 @@
 #' @example
 #' ans_tbl <- readRDS(file = "./apps/spinifex_study/www/ans_tbl.rds")
 #' tgt <- ans_tbl %>% dplyr::filter(sim_nm == "EEV_p6_33_66_rep2")
-#' df_longer <- pivot_longer_resp_ans_tbl(tgt)
+#' dat_longer <- pivot_longer_resp_ans_tbl(tgt)
 #' 
 #' 
 #' ## Raw data
 #' raw <- readRDS("./raw_nick_pilot.rds")
 #' ## Filter to only task data
-#' df_active <- raw[raw$plot_active == TRUE, ]
+#' dat_active <- raw[raw$plot_active == TRUE, ]
 #' ## Read and join ans_tbl, clean up 0 to NA on v5/v6.
 #' ans_tbl   <- readRDS(file = "../../apps/spinifex_study/www/ans_tbl.rds")
-#' resp_ans  <- dplyr::left_join(x = df_active, y =  ans_tbl, by = "sim_nm") %>%
+#' resp_ans  <- dplyr::left_join(x = dat_active, y =  ans_tbl, by = "sim_nm") %>%
 #'     mutate(v5_resp = ifelse(p_dim == "p4", NA_integer_, v5_resp),
 #'            v6_resp = ifelse(p_dim == "p4", NA_integer_, v6_resp))
 #' pivot_longer_resp_ans_tbl(resp_ans)
 #'
 #'
 ## Pivot variables longer and join (cbind)
-pivot_longer_resp_ans_tbl <- function(df){
-  ## Fix col if df was only from ans_tbl needed
-  if(all(c("key", "v1_resp", "v1_marks") %in% colnames(df)) == FALSE){
-    message("df was only from ans_tbl, fixing columns")
-    df <- df %>%  dplyr::mutate(.keep = "all",
+pivot_longer_resp_ans_tbl <- function(dat){
+  ## Fix col if dat was only from ans_tbl needed
+  if(all(c("key", "v1_resp", "v1_marks") %in% colnames(dat)) == FALSE){
+    message("dat was only from ans_tbl, fixing columns")
+    dat <- dat %>%  dplyr::mutate(.keep = "all",
                                 key = sim_nm,
-                                v1_resp = 0,
-                                v2_resp = 0,
-                                v3_resp = 0,
-                                v4_resp = 0,
-                                v5_resp = 0,
-                                v6_resp = 0,
-                                v1_marks = 0,
-                                v2_marks = 0,
-                                v3_marks = 0,
-                                v4_marks = 0,
-                                v5_marks = 0,
-                                v6_marks = 0)
+                                v1_resp = 0L,
+                                v2_resp = 0L,
+                                v3_resp = 0L,
+                                v4_resp = 0L,
+                                v5_resp = 0L,
+                                v6_resp = 0L,
+                                v1_marks = 0L,
+                                v2_marks = 0L,
+                                v3_marks = 0L,
+                                v4_marks = 0L,
+                                v5_marks = 0L,
+                                v6_marks = 0L)
   }
   
   
   ## Pivot each var longer and cbind  back together at end.
-  resp_longer <- df %>%
+  resp_longer <- dat %>%
     dplyr::select(c(key, sim_nm, bar, v1_resp:v6_resp)) %>%
     tidyr::pivot_longer(cols = v1_resp:v6_resp,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "resp",
                         values_drop_na = TRUE) %>%
-    dplyr::mutate(var_num = as.factor(substr(var_num, 2, 2)))
-  marks_longer <- df %>% 
+    dplyr::mutate(var_num = as.factor(substr(var_num, 2L, 2L)))
+  marks_longer <- dat %>% 
     dplyr::select(c(key:v6_marks)) %>%
     tidyr::pivot_longer(cols = v1_marks:v6_marks,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "marks",
                         values_drop_na = TRUE) %>%
-    dplyr::mutate(var_num = as.factor(substr(var_num, 2, 2)))
-  signal_longer <- df %>% 
+    dplyr::mutate(var_num = as.factor(substr(var_num, 2L, 2L)))
+  signal_longer <- dat %>% 
     dplyr::select(c(key, sim_nm, v1_signal:v6_signal)) %>%
     tidyr::pivot_longer(cols = v1_signal:v6_signal,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "signal",
                         values_drop_na = TRUE) %>%
-    dplyr::mutate(var_num = as.factor(substr(var_num, 2, 2)))
-  diff_longer <- df %>% 
+    dplyr::mutate(var_num = as.factor(substr(var_num, 2L, 2L)))
+  diff_longer <- dat %>% 
     dplyr::select(c(key, sim_nm, v1_diff:v6_diff)) %>%
     tidyr::pivot_longer(cols = v1_diff:v6_diff,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "diff",
                         values_drop_na = TRUE) %>%
-    dplyr::mutate(var_num = as.factor(substr(var_num, 2, 2)))
-  weight_longer <- df %>% 
+    dplyr::mutate(var_num = as.factor(substr(var_num, 2L, 2L)))
+  weight_longer <- dat %>% 
     dplyr::select(c(key, sim_nm, v1_weight:v6_weight)) %>%
     tidyr::pivot_longer(cols = v1_weight:v6_weight,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "weight",
                         values_drop_na = TRUE) %>%
-    dplyr::mutate(var_num = as.factor(substr(var_num, 2, 2)))
+    dplyr::mutate(var_num = as.factor(substr(var_num, 2L, 2L)))
   dat <- NA
   if(all.equal(
     nrow(resp_longer), nrow(marks_longer),
@@ -86,8 +86,8 @@ pivot_longer_resp_ans_tbl <- function(df){
   )){
     ## cbind(), left_join not working.
     dat <- cbind(
-      resp_longer, marks_longer[, 4], signal_longer[, 4],
-      diff_longer[, 4], weight_longer[, 4])
+      resp_longer, marks_longer[, 4L], signal_longer[, 4L],
+      diff_longer[, 4L], weight_longer[, 4L])
   }else{error("!!!all nrow() not equal!!!")}
   
   ret <- dat %>% dplyr::group_by(sim_nm, var_num) %>%
