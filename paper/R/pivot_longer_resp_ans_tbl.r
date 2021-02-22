119bcd0327a632134e79b99afd614ff01ec0cc90
Nick Spyrison <spyrison@gmail.com>
2021-02-22 20:41:04 +1100

    consolidating analysis on the prolifico 100.

--- paper/R/pivot_longer_resp_ans_tbl.r
+++ paper/R/pivot_longer_resp_ans_tbl.r
@@ -1,3 +1,5 @@
+`%>%` <- dplyr::`%>%`
+
 #' @example
 #' ans_tbl <- readRDS(file = "./apps/spinifex_study/www/ans_tbl.rds")
 #' tgt <- ans_tbl %>% dplyr::filter(sim_nm == "EEV_p6_33_66_rep2")
@@ -14,8 +16,7 @@
 #'     mutate(v5_resp = ifelse(p_dim == "p4", NA_integer_, v5_resp),
 #'            v6_resp = ifelse(p_dim == "p4", NA_integer_, v6_resp))
 #' pivot_longer_resp_ans_tbl(resp_ans)
-#'
-#'
+
 ## Pivot variables longer and join (cbind)
 pivot_longer_resp_ans_tbl <- function(dat){
   ## Fix col if dat was only from ans_tbl needed
@@ -44,6 +45,7 @@ pivot_longer_resp_ans_tbl <- function(dat){
                     v4_marks    = 0L,
                     v5_marks    = 0L,
                     v6_marks    = 0L,
+                    sec_to_resp = 0L,
                     sec_on_pg   = NA,
                     input_inter = NA,
                     resp_inter  = NA)
@@ -53,35 +55,35 @@ pivot_longer_resp_ans_tbl <- function(dat){
   resp_longer <- dat %>%
     dplyr::select(c(key, prolific_id, sim_nm,
                     period, eval, factor, vc, p_dim, location,
-                    input_inter, resp_inter, sec_on_pg, bar,
+                    input_inter, resp_inter, sec_to_resp, sec_on_pg, bar,
                     v1_resp:v6_resp)) %>%
     tidyr::pivot_longer(cols = v1_resp:v6_resp,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "resp",
                         values_drop_na = TRUE)
-  marks_longer <- dat %>% 
+  marks_longer <- dat %>%
     dplyr::select(c(key, sim_nm, v1_marks:v6_marks)) %>%
     tidyr::pivot_longer(cols = v1_marks:v6_marks,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "marks",
                         values_drop_na = TRUE)
-  signal_longer <- dat %>% 
+  signal_longer <- dat %>%
     dplyr::select(c(key, sim_nm, v1_signal:v6_signal)) %>%
     tidyr::pivot_longer(cols = v1_signal:v6_signal,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "signal",
                         values_drop_na = TRUE)
-  diff_longer <- dat %>% 
+  diff_longer <- dat %>%
     dplyr::select(c(key, sim_nm, v1_diff:v6_diff)) %>%
     tidyr::pivot_longer(cols = v1_diff:v6_diff,
                         names_to = "var_num",
                         names_prefix  = "var_num",
                         values_to = "diff",
                         values_drop_na = TRUE)
-  weight_longer <- dat %>% 
+  weight_longer <- dat %>%
     dplyr::select(c(key, sim_nm, v1_weight:v6_weight)) %>%
     tidyr::pivot_longer(cols = v1_weight:v6_weight,
                         names_to = "var_num",
@@ -116,17 +118,23 @@ pivot_longer_resp_ans_tbl <- function(dat){
   return(ret)
 }
 
-
+#' @example
+#' ans_tbl <- readRDS(file = "./apps/spinifex_study/www/ans_tbl.rds")
+#' tgt <- ans_tbl %>% dplyr::filter(sim_nm == "EEV_p6_33_66_rep2")
+#' dat_longer <- pivot_longer_resp_ans_tbl(tgt)
+#' dat_longer_agg <- aggregate_task_vars(dat_longer)
+#' 
 ## Aggregate to the task grain.
 aggregate_task_vars <- function(df_long){
-  df_long %>% 
+  df_long %>%
     dplyr::group_by(key, prolific_id, sim_nm, factor, period, eval, is_training, vc, p_dim, location) %>%
     dplyr::summarise(task_input_inter = mean(input_inter),
                      task_resp_inter = mean(resp_inter),
+                     max_sec_to_resp = max(sec_to_resp),
                      max_sec_on_pg = max(sec_on_pg),
                      cnt_resp = sum(resp),
                      task_marks = sum(marks),
                      z_weight_check = sum(weight)
-    ) %>% dplyr::ungroup() %>% 
+    ) %>% dplyr::ungroup() %>%
     tibble::as_tibble()
 }
